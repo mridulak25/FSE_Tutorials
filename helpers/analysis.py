f2c0c0b2afa8ea3fb0b8168a09a150f5cc0e2988
mridulak25 <kumar.mridula25@gmail.com>
2026-03-04 13:56:44 +0200

    Completed tut-7

--- helpers/analysis.py
+++ helpers/analysis.py
@@ -165,7 +165,17 @@ def get_category_names():
 
 #TODO Complete the function below, use the helper function provided to convert to dicts otherwise tests will fail.
 def get_transactions_by_category(category_name: str):
-    pass
+    session = get_session()
+    try:
+        stmt = select(Category).where(Category.name == category_name)
+        category = session.execute(stmt).scalars().first()
+        if not category:
+            logger.info(f"No category found with name '{category_name}'.")
+            return []
+        transactions = category.transactions
+        return convert_transactions_to_dict(transactions)
+    finally:
+        session.close()
 
 #NOTE: I have added this helper function to convert Transaction objects to dictionaries for JSON serialization, for use in the function above.
 def convert_transactions_to_dict(transactions):
